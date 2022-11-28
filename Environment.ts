1973c0bb10e6e55a289e7a2d0fb3315f1d860870
Oluwatobi Akanji <tobi@542.digital>
2022-11-28 23:54:07 +0100

    chore: updated syntax

--- Environment.ts
+++ Environment.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv';
-dotenv.config()
+dotenv.config();
 
 export const getPrivateKey = () => process.env.PRIVATE_KEY;
 export const getApiUrl = () => process.env.API_URL;
